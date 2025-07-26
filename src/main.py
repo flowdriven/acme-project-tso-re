7dcb9f7cff1fc818d615360be8127241eb3097f3
flowdriven <chris@flowdriven.dev>
2025-07-26 16:35:32 +0200

    add project paths retrieve

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 import os
 
-from utils.utils import setup_logger
+from utils.utils import setup_logger, get_paths
 
 dataset_list = os.getenv("DATASET_LIST").split(",")
 prefix = os.getenv("PREFIX")
 
+def read_file_as_string(path: str) -> str:
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File not found: {path}")
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+    
 def main():
     logger = setup_logger()
 
-    # read xml as strings
+    for dataset in dataset_list:
+        paths = get_paths(dataset) 
+        #logger.info(f"Starting XML validation: {paths["xml_path"]} against {paths["xsd_path"]}")
 
-    # read xsd as strings 
+        xml_string = read_file_as_string(paths["xml_path"])
+        xsd_string = read_file_as_string(paths["xsd_path"])
+
 
     # validate xml against xsd schemas 
 
